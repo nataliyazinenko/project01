f6e5d49c71cf4bc306a2a310e27195c23bee524a
nataliyazinenko <n.zinenko@gmail.com>
2024-08-24 16:43:48 +0100

    implement /api endpoint and test

--- db/app.js
+++ db/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
-const { getAllTopics } = require("./controllers");
+const { getAllTopics, getEndpointsDocumentation } = require("./controllers");
 
 const app = express();
 
 app.use(express.json());
+
+app.get("/api", getEndpointsDocumentation);
 
 app.get("/api/topics", getAllTopics);
 
