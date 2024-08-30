9d660c72a762514ef125ecc02d58c30a4369a075
nataliyazinenko <n.zinenko@gmail.com>
2024-08-30 12:38:02 +0100

    refactor and test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -43,17 +43,18 @@ describe("/api/articles/:article_id tests", () => {
       .get("/api/articles/1")
       .expect(200)
       .then((response) => {
-        expect(response.body.article.article_id).toBe(1);
-        expect(response.body.article).toHaveProperty("author");
-        expect(response.body.article).toHaveProperty("title");
-        expect(response.body.article).toHaveProperty("article_id");
-        expect(response.body.article).toHaveProperty("body");
-        expect(response.body.article).toHaveProperty("topic");
-        expect(response.body.article).toHaveProperty("created_at");
-        expect(response.body.article).toHaveProperty("votes");
-        expect(response.body.article).toHaveProperty("article_img_url");
-        expect(response.body.article).toHaveProperty("comment_count");
-        expect(response.body.article.comment_count).toBe(11);
+        expect(response.body.article).toMatchObject({
+          author: "butter_bridge",
+          title: "Living in the shadow of a great man",
+          article_id: 1,
+          body: "I find this existence challenging",
+          topic: "mitch",
+          created_at: "2020-07-09T20:11:00.000Z",
+          votes: 100,
+          article_img_url:
+            "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+          comment_count: 11,
+        });
       });
   });
 
